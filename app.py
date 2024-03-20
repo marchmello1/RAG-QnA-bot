9cb93420e00be7ad45e52db9c0af0719545af861
Mohammad Agwan <89778018+marchmello1@users.noreply.github.com>
2024-03-20 23:26:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-import os
-from dotenv import load_dotenv
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import faiss
-from langchain.prompts import PromptTemplate
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain_community.document_loaders import PDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import create_retrieval_chain
 from langchain.chat_models import ChatOpenAI
-from htmlTemplates import css, bot_template, user_template
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.prompts import ChatPromptTemplate
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,73 +12,22 @@ load_dotenv()
 # Load OpenAI API key from Streamlit secrets
 openai_api_key = st.secrets["streamlit"]["openai_api_key"]
 
-# Custom prompt template for rephrasing follow-up questions
-custom_template = """
-Given the following conversation and a follow-up question, rephrase the follow-up question to be a standalone question, in its original language.
-Chat History:
-{chat_history}
-Follow-Up Input: {question}
-Standalone question:
-"""
-
-# Prompt template instance
-CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
-
 def get_pdf_text(docs):
     text = ""
     for pdf in docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-            text += "\n"  # Add a newline character to separate text from different pages
+        text += PDFLoader(pdf).load()
     return text
 
-
-# Convert text to chunks
 def get_chunks(raw_text):
-    text_splitter = CharacterTextSplitter(separator="\n",
-                                          chunk_size=1000,
-                                          chunk_overlap=200,
-                                          length_function=len)   
-    chunks = text_splitter.split_text(raw_text)
-    return chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_text(raw_text)
+    return documents
 
-# Generate vector store
-def get_vectorstore(chunks):
+def get_vectorstore(documents):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                       model_kwargs={'device':'cpu'})
-    vectorstore = faiss.FAISS.from_texts(texts=chunks, embedding=embeddings)
+                                       model_kwargs={'device': 'cpu'})
+    vectorstore = FAISS.from_documents(documents, embeddings)
     return vectorstore
-
-# Generate conversation chain  
-def get_conversationchain(vectorstore, openai_api_key):
-    llm = ChatOpenAI(temperature=0.2, openai_api_key=openai_api_key)
-    memory = ConversationBufferMemory(memory_key='chat_history', 
-                                      return_messages=True,
-                                      output_key='answer') 
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-                                llm=llm,
-                                retriever=vectorstore.as_retriever(),
-                                condense_question_prompt=CUSTOM_QUESTION_PROMPT,
-                                memory=memory)
-    return conversation_chain
-
-# Generate response from user queries and display them accordingly
-def handle_question(question, openai_api_key):
-    if st.session_state.conversation:
-        response = st.session_state.conversation({'question': question})
-        if response["answer"]:
-            st.session_state.chat_history = response["chat_history"]
-            for i, msg in enumerate(st.session_state.chat_history):
-                if i % 2 == 0:
-                    st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
-                else:
-                    st.write(bot_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
-            return
-
-    llm = ChatOpenAI(temperature=0.2, openai_api_key=openai_api_key)
-    response = llm.predict(question)  # Use predict() method to generate response
-    st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
 
 def main():
     st.set_page_config(page_title="Picostone QnA bot", page_icon=":robot_face:", layout="wide")
@@ -114,15 +58,54 @@ def main():
                     raw_text = get_pdf_text(docs)
                     
                     # Get the text chunks
-                    text_chunks = get_chunks(raw_text)
+                    documents = get_chunks(raw_text)
                     
                     # Create vectorstore
-                    vectorstore = get_vectorstore(text_chunks)
+                    vectorstore = get_vectorstore(documents)
                     
-                    # Create conversation chain
-                    st.session_state.conversation = get_conversationchain(vectorstore, openai_api_key)  # Pass the API key here
+                    # Define LLM
+                    llm = ChatOpenAI(openai_api_key=openai_api_key)
+
+                    # Define prompt template
+                    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+                    <context>
+                    {context}
+                    </context>
+
+                    Question: {input}""")
+                    
+                    # Create a retrieval chain to answer questions
+                    retrieval_chain = create_retrieval_chain(vectorstore.as_retriever(), llm, prompt)
+                    
+                    # Store conversation chain in session state
+                    st.session_state.conversation = retrieval_chain
                 else:
                     st.warning("No PDF files uploaded. Continuing conversation without searching from PDFs.")
+
+# Function to handle question
+def handle_question(question, openai_api_key):
+    # Check if the conversation chain is initialized
+    if st.session_state.conversation:
+        response = st.session_state.conversation({'question': question})
+        if response["answer"]:
+            st.session_state.chat_history = response["chat_history"]
+            for i, msg in enumerate(st.session_state.chat_history):
+                if i % 2 == 0:
+                    st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
+                else:
+                    st.write(bot_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
+            return
+        else:
+            # If no relevant documents found, fallback to LLM model
+            llm = ChatOpenAI(openai_api_key=openai_api_key)
+            response = llm.predict(question)
+            st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
+    else:
+        # If conversation chain hasn't been initialized yet, use LLM model directly
+        llm = ChatOpenAI(openai_api_key=openai_api_key)
+        response = llm.predict(question)
+        st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()

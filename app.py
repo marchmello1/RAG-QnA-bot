5421e2783d1c6593d2df4ba6d9673eb9f174d586
Mohammad Agwan <89778018+marchmello1@users.noreply.github.com>
2024-06-10 21:06:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import csv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -39,80 +38,64 @@ def get_pdf_text(docs):
             text += "\n"  # Add a newline character to separate text from different pages
     return text
 
+
+# Convert text to chunks
 def get_chunks(raw_text):
     text_splitter = CharacterTextSplitter(separator="\n",
                                           chunk_size=1000,
                                           chunk_overlap=200,
-                                          length_function=len)
+                                          length_function=len)   
     chunks = text_splitter.split_text(raw_text)
     return chunks
 
+# Generate vector store
 def get_vectorstore(chunks):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                       model_kwargs={'device': 'cpu'})
+                                       model_kwargs={'device':'cpu'})
     vectorstore = faiss.FAISS.from_texts(texts=chunks, embedding=embeddings)
     return vectorstore
 
+# Generate conversation chain  
 def get_conversationchain(vectorstore, openai_api_key):
     llm = ChatOpenAI(temperature=0.2, openai_api_key=openai_api_key)
-    memory = ConversationBufferMemory(memory_key='chat_history',
+    memory = ConversationBufferMemory(memory_key='chat_history', 
                                       return_messages=True,
-                                      output_key='answer')
+                                      output_key='answer') 
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        condense_question_prompt=CUSTOM_QUESTION_PROMPT,
-        memory=memory)
+                                llm=llm,
+                                retriever=vectorstore.as_retriever(),
+                                condense_question_prompt=CUSTOM_QUESTION_PROMPT,
+                                memory=memory)
     return conversation_chain
 
-# Save dataset to CSV file
-def save_dataset(question, answer):
-    if "dataset" not in st.session_state:
-        st.session_state.dataset = []
-
-    st.session_state.dataset.append({"question": question, "answer": answer})
-    with open("dataset.csv", "w", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=["question", "answer"])
-        writer.writeheader()
-        writer.writerows(st.session_state.dataset)
-
-# Load dataset from CSV file
-def load_dataset():
-    if "dataset" not in st.session_state:
-        st.session_state.dataset = []
-
-    if os.path.exists("dataset.csv"):
-        with open("dataset.csv", "r", encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            st.session_state.dataset = [row for row in reader]
-
+# Generate response from user queries and display them accordingly
 def handle_question(question, openai_api_key):
-    try:
-        if st.session_state.conversation:
-            response = st.session_state.conversation({'question': question})
-            if response["answer"]:
-                st.session_state.chat_history = response["chat_history"]
-                for i, msg in enumerate(st.session_state.chat_history):
-                    if i % 2 == 0:
-                        st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
-                    else:
-                        st.write(bot_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
-                save_dataset(question, response["answer"])
-                return
-
-        if st.session_state.conversation and response.get("answer", "").startswith("I don't know"):
-            llm = ChatOpenAI(temperature=0.2, openai_api_key=openai_api_key)
-            response = llm.predict(question)
-            st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
-            save_dataset(question, response)
+    # Check if there's conversation history available
+    if st.session_state.conversation:
+        response = st.session_state.conversation({'question': question})
+        if response["answer"]:
+            # If answer found in conversation history, display it
+            st.session_state.chat_history = response["chat_history"]
+            for i, msg in enumerate(st.session_state.chat_history):
+                if i % 2 == 0:
+                    st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
+                else:
+                    st.write(bot_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
             return
 
+    # Check if the question is specific and related to the uploaded PDF documents
+    if st.session_state.conversation and response.get("answer", "").startswith("I don't know"):
+        # If specific question not found in processed documents, use LLM to respond
         llm = ChatOpenAI(temperature=0.2, openai_api_key=openai_api_key)
         response = llm.predict(question)
         st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
-        save_dataset(question, response)
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+        return
+
+    # If none of the above conditions are met, use the LLM to generate a response
+    llm = ChatOpenAI(temperature=0.2, openai_api_key=openai_api_key)
+    response = llm.predict(question)
+    st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
+
 
 def main():
     st.set_page_config(page_title="Picostone QnA bot", page_icon=":robot_face:", layout="wide")
@@ -122,35 +105,39 @@ def main():
         st.session_state.conversation = None
 
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
-
+        st.session_state.chat_history = None
+    
     if "vectorstore" not in st.session_state:
         st.session_state.vectorstore = None
 
-    load_dataset()
-
     st.markdown("<h1 style='text-align: center; color: #075E54;'>QnA Bot</h1>", unsafe_allow_html=True)
     question = st.text_input("Ask a question")
-
+    
     if question:
-        handle_question(question, openai_api_key)
+        handle_question(question, openai_api_key)  # Pass the API key here
     else:
         st.warning("Type a question to start the conversation.")
-
+    
     with st.sidebar:
         st.subheader("Upload Documents")
         docs = st.file_uploader("Upload PDF documents", accept_multiple_files=True)
-
+        
         if st.button("Process Documents"):
             with st.spinner("Processing"):
                 if docs:
+                    # Get the pdf
                     raw_text = get_pdf_text(docs)
+                    
+                    # Get the text chunks
                     text_chunks = get_chunks(raw_text)
+                    
+                    # Create vectorstore
                     st.session_state.vectorstore = get_vectorstore(text_chunks)
-                    st.session_state.conversation = get_conversationchain(st.session_state.vectorstore, openai_api_key)
+                    
+                    # Create conversation chain
+                    st.session_state.conversation = get_conversationchain(st.session_state.vectorstore, openai_api_key)  # Pass the API key here
                 else:
                     st.warning("No PDF files uploaded. Continuing conversation without searching from PDFs.")
 
-if __name__ == '__main__':
-    main()
-
+if _name_ == '_main_':
+    main()

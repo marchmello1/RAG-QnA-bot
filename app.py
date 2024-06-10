b3e38937c87403d7137bb331a750b69d3ff66051
Mohammad Agwan <89778018+marchmello1@users.noreply.github.com>
2024-06-10 22:18:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def get_pdf_text(docs):
             text += "\n"  # Add a newline character to separate text from different pages
     return text
 
-
 # Convert text to chunks
 def get_chunks(raw_text):
     text_splitter = CharacterTextSplitter(separator="\n",
@@ -76,7 +75,7 @@ def handle_question(question, openai_api_key):
         if response["answer"]:
             # If answer found in conversation history, display it
             st.session_state.chat_history = response["chat_history"]
-            for i, msg in enumerate(st.session_state.chat_history):
+            for i, msg in enumerate(reversed(st.session_state.chat_history)):
                 if i % 2 == 0:
                     st.write(user_template.replace("{{MSG}}", msg.content), unsafe_allow_html=True)
                 else:
@@ -95,7 +94,6 @@ def handle_question(question, openai_api_key):
     llm = ChatOpenAI(temperature=0.2, openai_api_key=openai_api_key)
     response = llm.predict(question)
     st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
-
 
 def main():
     st.set_page_config(page_title="Picostone QnA bot", page_icon=":robot_face:", layout="wide")
@@ -139,5 +137,5 @@ def main():
                 else:
                     st.warning("No PDF files uploaded. Continuing conversation without searching from PDFs.")
 
-if _name_ == '_main_':
-    main()
+if __name__ == '__main__':
+    main()
